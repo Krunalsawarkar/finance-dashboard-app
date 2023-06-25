4e8354634f5a3747f0e1c7c046f925e78bac54c4
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-25 11:55:31 +0530

    Working Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./scenes/navbar";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard"
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,7 +16,7 @@ const App = () => {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
               <Navbar />
               <Routes>
-                  <Route path="/" element={<div>dashboard page</div>} />
+                  <Route path="/" element={<Dashboard />} />
                   <Route path="/predictions" element = {<div>predictions page</div>} />
               </Routes>
           </Box>
